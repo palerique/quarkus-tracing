ce61d925e8fb0408662b64b4c2e7dc36d4ba79db
C1329263 Paulo Henrique Lerbach Rodrigues <c1329263@interno.bb.com.br>
2023-05-18 14:56:35 -0300

    Adiciona mensagens no log com o trace id e define o host para o GELF/Graylog

--- iniciar-projeto.sh
+++ iniciar-projeto.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-EXTENSIONS='resteasy-reactive-jackson,
-rest-client-reactive-jackson,
-resteasy-reactive,
-quarkus-hibernate-reactive-panache,
-quarkus-opentelemetry,
-smallrye-reactive-messaging-kafka,
-micrometer-registry-prometheus,
-logging-gelf'
-
-quarkus create app br.com.palerique:quarkus-servico-a \
-  --extension="$EXTENSIONS" \
-  --gradle-kotlin-dsl
+#EXTENSIONS='resteasy-reactive-jackson,
+#rest-client-reactive-jackson,
+#resteasy-reactive,
+#quarkus-hibernate-reactive-panache,
+#quarkus-opentelemetry,
+#smallrye-reactive-messaging-kafka,
+#micrometer-registry-prometheus,
+#logging-gelf'
+#
+#quarkus create app br.com.palerique:quarkus-servico-a \
+#  --extension="$EXTENSIONS" \
+#  --gradle-kotlin-dsl
 
 #quarkus create app br.com.palerique:quarkus-servico-b \
 #  --extension="$EXTENSIONS" \
