a10cbd93a3ee4d3864903f483f351a51ffefe34f
Paulo Rodrigues <palerique@gmail.com>
2023-05-23 16:53:38 -0300

    preparando projeto inicial

--- quarkus-service-a/src/native-test/java/br/com/palerique/InicioResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.palerique;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class InicioResourceIT extends InicioResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- quarkus-service-piada/src/native-test/java/br/com/palerique/PiadaResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.palerique;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class PiadaResourceIT extends PiadaResourceTest {
-    // Execute the same tests but in packaged mode.
-}
