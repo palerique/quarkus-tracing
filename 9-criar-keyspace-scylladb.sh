7d3c3fb9364ea0c94442d6ee4e42c3d1f051f4a5
C1329263 Paulo Henrique Lerbach Rodrigues <c1329263@interno.bb.com.br>
2023-05-25 12:52:26 -0300

    preparando para a apresentacao

--- 9-criar-keyspace-scylladb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-docker compose exec scylla cqlsh -e "DROP KEYSPACE IF EXISTS piadas;"
-docker compose exec scylla cqlsh -e "CREATE KEYSPACE piadas WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};"
-docker compose exec scylla cqlsh -e "CREATE TABLE piadas.piadas (id uuid, texto text,PRIMARY KEY (id));"
